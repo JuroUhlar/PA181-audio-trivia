8ed6df1e15ffb4f070255c9e3ea06d1ca7e28ecc
Martin <422491@mail.muni.cz>
2020-06-16 16:45:34 +0200

    Roughly done
    Say question and it gives you next question (or use button). TTS (reading the question) temporarily disabled (because of the limit)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,18 @@ import './App.css';
 import Speak from './components/Speak';
 // @ts-ignore
 import Recorder from 'react-mp3-recorder'
+import { runInThisContext } from 'vm';
+const levenshtein = require('js-levenshtein');
 
 interface AppState {
-  text: string
-  recordedText: string
+  text: string,
+  recordedText: string,
+  question: string,
+  incorrectAnswers: string[],
+  correctAnswer: string,
+  mixedAnswers: string[],
+  outcome: string,
 }
-
-const levenshtein = require('js-levenshtein');
 
 export class App extends React.Component<any, AppState> {
 
@@ -23,7 +28,8 @@ export class App extends React.Component<any, AppState> {
 	  question: '',
 	  incorrectAnswers: [],
 	  correctAnswer: '',
-	  mixedAnswers: [],
+    mixedAnswers: [],
+    outcome: '',
     }
   }
 
@@ -57,15 +63,67 @@ export class App extends React.Component<any, AppState> {
           .then(response => {
             if(response.result.results.length !== 0){
               this.setState(() => ({
-                recordedText: response.result.results[response.result.results.length-1].alternatives[0].transcript
+                recordedText: response.result.results[response.result.results.length-1].alternatives[0].transcript,
+                outcome: this.getOutcome(response.result.results[response.result.results.length-1].alternatives[0].transcript)//response.result.results[response.result.results.length-1].alternatives[0].transcript"")
               }))}
           });
-		this.state.mixedAnswers.map((ans) => {
-			console.log(levenshtein(this.state.recordedText, ans));
-		});
     }
   };
-
+  getOutcome(recordedText)
+  {
+  switch (recordedText.trim()) {
+    case "a":
+    case "hey":
+    case "hey you":
+    case "our":
+    case "A.":
+    case "eight":
+      if (this.getIndex(this.state.correctAnswer, this.state.mixedAnswers) == 0)
+        return ("Correct!");
+      else 
+        return ("Incorrect!");
+    case "b":
+    case "bee":
+    case "be":
+    case "e":
+    case "B.":
+      if (this.getIndex(this.state.correctAnswer, this.state.mixedAnswers) == 1)
+        return ("Correct!");
+      else 
+        return ("Incorrect!");
+    case "c":
+    case "see":
+    case "sea":
+    case "ce":
+    case "C.":
+      if (this.getIndex(this.state.correctAnswer, this.state.mixedAnswers) == 2)
+        return ("Correct!");
+      else 
+        return ("Incorrect!");
+    case "d":
+    case "deer":
+    case "dear":
+    case "de":
+    case "D.":
+      if (this.getIndex(this.state.correctAnswer, this.state.mixedAnswers) == 3)
+        return ("Correct!");
+      else 
+        return ("Incorrect!");
+    case "Question.":
+    case "Questions.":
+    case "question":
+    case "questions":
+    case "Christian.":
+    case "christian":
+    {
+      this.getQuestion();
+      return ("");
+    }  
+    default: 
+        return('Unable to process! Please, try to record your answer again!');
+  }
+  return('Nerozpoznano');
+}
   _onRecordingError = (err: any) => {
     console.log('recording error', err)
   };
@@ -73,22 +131,36 @@ export class App extends React.Component<any, AppState> {
   //  nevěděl jsem jak to rychle po par pokusech jednoduše rozkodovat, tak je to takto skarede
   //  takze to chce predelat
   getQuestion = () => {
+  
 	fetch('https://opentdb.com/api.php?amount=1&type=multiple')
 		.then(response => response.json())
 		.then(data => this.setState({ 
-			question: data.results[0].question.replace(/&quot;/g, '"').replace(/&#039;/g, '\''),
+      recordedText:"",
+      question: data.results[0].question.replace(/&quot;/g, '"').replace(/&#039;/g, '\''),
 			correctAnswer: data.results[0].correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, '\''),
 			incorrectAnswers: data.results[0].incorrect_answers.map(x => x.replace(/&quot;/g, '"').replace(/&#039;/g, '\'')),
 			mixedAnswers: [...data.results[0].incorrect_answers.map(x => x.replace(/&quot;/g, '"').replace(/&#039;/g, '\''))]
 						  .concat(data.results[0].correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, '\'')).sort(() => 0.5 - Math.random())
 		}));
-	}
+  };
+  
+   getIndex(value, arr) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] === value) {
+            return i;
+        }
+    }
+    return -1; 
+};
 
   render() {
+    const answers = this.state.mixedAnswers.map((item) =>
+    <li>{item}</li>
+    );
     return (
       <div className="App">
         <h1>Audio trivia </h1>
-
+        Toto kdyz zkusim schovat, tak to haze error, nevim proc
         <form onSubmit={this.handleSubmit}>
           <label>
             Say something: <br/>
@@ -97,15 +169,15 @@ export class App extends React.Component<any, AppState> {
           <input type="submit" value="Submit" />
         </form>
 
-        <Speak text={this.state.text} />
+
 		
-        <div id='triviaAPI'>
-          <label>
+    <div id='triviaAPI'>
+    <label>
 			<br />
-			<h1> Get question from triviaAPI</h1> 
-			<button onClick={this.getQuestion}>Get question</button>
+			<h1> Get question from triviaAPI!</h1> 
+			  <button onClick={this.getQuestion}>Get question</button>
 			<br/> Question is: <br/>
-			<Speak text={this.state.question + " " + this.state.mixedAnswers.join("? or ") } />
+			<b>{this.state.question + " " + this.state.mixedAnswers.join("? or ") + "?"} </b>
           </label>
 		  
         </div>
@@ -116,8 +188,14 @@ export class App extends React.Component<any, AppState> {
           />
           <label>
             <br/> Click and Hold to record <br/>
+            Correct answer: {this.state.correctAnswer} <br/>
             You have said: <b>{this.state.recordedText}</b>
+            <br/>
+              Outcome: <b>{this.state.outcome}</b>
           </label>
+          <ol type="a">
+              {answers}
+            </ol>
         </div>
 
       </div>
